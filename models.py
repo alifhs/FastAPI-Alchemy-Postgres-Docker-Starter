a186fb3640460bae2ba230f68e40de3080dd1c51
alifhs <alif.hs@outlook.com>
2024-05-28 20:21:19 +0600

    add dependencies

---
+++ models.py
@@ -0,0 +1,13 @@
+from sqlalchemy import Column, Integer, String, MetaData
+from sqlalchemy.ext.declarative import declarative_base
+
+metadata = MetaData()
+
+Base = declarative_base(metadata=metadata)
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+    email = Column(String, unique=True, index=True)
