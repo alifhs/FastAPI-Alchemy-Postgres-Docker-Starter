585e47258581f907dd8749012d8450458b00f839
alifhs <alif.hs@outlook.com>
2024-05-31 20:24:23 +0600

    integrate redis

--- crud.py
+++ crud.py
@@ -1,8 +1,8 @@
 from sqlalchemy.orm import Session
 import models
 import schemas
-
-
+from redis_service import redis_client as redis
+import json
 def create_user(db: Session, user: schemas.UserCreate):
     db_user = models.User(name=user.name, email=user.email)
     db.add(db_user)
@@ -11,4 +11,17 @@ def create_user(db: Session, user: schemas.UserCreate):
     return db_user
 
 def get_user(db: Session, user_id: int):
-    return db.query(models.User).filter(models.User.id == user_id).first()
+    user_data =  redis.get(f"user:{user_id}")
+    if user_data:
+        # If user data is found in cache, return it
+        print("from redis", user_data)
+        return json.loads(user_data)
+
+    # If user data is not in cache, fetch it from the database
+    user = db.query(models.User).filter(models.User.id == user_id).first()
+
+    if user:
+        # Cache the user data in Redis
+        redis.set(f"user:{user_id}", json.dumps(user.to_dict()))
+
+    return user
--- models.py
+++ models.py
@@ -11,3 +11,10 @@ class User(Base):
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String, index=True)
     email = Column(String, unique=True, index=True)
+
+    def to_dict(self):
+            return {
+                'id': self.id,
+                'name': self.name,
+                'email': self.email
+            }
